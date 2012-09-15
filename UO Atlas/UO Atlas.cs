2f204743542306c141c03090c02f8419a5f7c825
trox085 <trox085@4c43571c-a11e-fb76-8e1c-3a807efea087>
2012-09-15 14:05:34 +0000

    Rewrote some of the code to help with memory concerns.  No functionality should have been affected.
    Added the ability to import UOAM labels from .map files.  They will be stored in an embedded FireBird database.

--- UO Atlas/UO Atlas.cs
+++ UO Atlas/UO Atlas.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO.Compression;
 using System.Windows.Forms;
 using System.IO;
@@ -72,33 +73,11 @@ namespace UO_Atlas
         /// <summary>
         /// Gets the connection string for database containing the map labels.
         /// </summary>
-        public static string MapLabelsDatabaseConnectionString
+        internal static string MapLabelsDatabaseConnectionString
         {
             get
             {
-                string dbPath = Path.Combine(Atlas.MapLabelsFolder, "database.fdb");
-
-                
-
-                        //using(MemoryStream ms = new MemoryStream(Resources.FireBirdFiles))
-                        //{
-
-
-                            //DeflateStream zipStream = new DeflateStream(ms, CompressionMode.Decompress);
-
-                            //int bytesRead;
-                            //do
-                            //{
-                            //    bytesRead = zipStream.Read(
-                            //}
-
-                            //byte[] decompressedBuffer = new byte[streamData.Length + 100];
-                            //// Use the ReadAllBytesFromStream to read the stream.
-                            //int totalCount = DeflateTest.ReadAllBytesFromStream(zipStream, decompressedBuffer);
-                            //Console.WriteLine("Decompressed {0} bytes", totalCount);
-                        //}
-                //}
-                
+                string dbPath = Path.Combine(MapLabelsFolder, "data.fdb");
 
                 FbConnectionStringBuilder connectionStringBuilder = new FbConnectionStringBuilder();
                 connectionStringBuilder.Database = dbPath;
@@ -114,6 +93,173 @@ namespace UO_Atlas
                 }
 
                 return connectionString;
+            }
+        }
+
+
+
+
+
+
+
+        private static void EnsureFireBirdSetup()
+        {
+            string[] requiredFiles = new string[]
+                                         {
+                                             Path.Combine(Application.StartupPath, "FirebirdSql.Data.FirebirdClient.dll"),
+                                             Path.Combine(Application.StartupPath, "fbembed.dll"),
+                                             Path.Combine(Application.StartupPath, "icudt30.dll"),
+                                             Path.Combine(Application.StartupPath, "icuin30.dll"),
+                                             Path.Combine(Application.StartupPath, "icuuc30.dll"),
+                                             Path.Combine(Application.StartupPath, "ib_util.dll"),
+                                             Path.Combine(Application.StartupPath, "firebird.conf"),
+                                             Path.Combine(Application.StartupPath, "firebird.msg"),
+                                         };
+            bool allFilesExist = true;
+
+            foreach (string requiredFile in requiredFiles)
+            {
+                if (!File.Exists(requiredFile))
+                {
+                    allFilesExist = false;
+                    break;
+                }
+            }
+            if (allFilesExist)
+            {
+                return;
+            }
+
+
+            string fireBirdZipFile = Path.Combine(Application.StartupPath, "fireBirdFiles.zip");
+            using (FileStream outStream = new FileStream(fireBirdZipFile, FileMode.Create))
+            {
+                byte[] streamData = Resources.FireBirdFiles;
+                outStream.Write(streamData, 0, streamData.Length);
+            }
+
+            EnsureShell32();
+
+            ExtractFireBirdFiles(fireBirdZipFile);
+        }
+
+
+
+        private static void EnsureShell32()
+        {
+            string shell32ReferenceFile = Path.Combine(Application.StartupPath, "Interop.Shell32.dll");
+            if (File.Exists(shell32ReferenceFile))
+            {
+                return;
+            }
+
+            using (FileStream outStream = new FileStream(shell32ReferenceFile, FileMode.Create))
+            {
+                byte[] streamData = Resources.Shell32;
+                outStream.Write(streamData, 0, streamData.Length);
+            }
+        }
+
+
+
+        private static void ExtractFireBirdFiles(string zipFile)
+        {
+            string zipFileExtractionPath = Path.Combine(Application.StartupPath, Path.GetFileNameWithoutExtension(zipFile));
+            Utility.EnsureDirectory(zipFileExtractionPath);
+
+            Shell32.ShellClass shellClass = new Shell32.ShellClass();
+            Shell32.Folder srcFolder = shellClass.NameSpace(zipFile);
+            Shell32.Folder destFolder = shellClass.NameSpace(zipFileExtractionPath);
+            destFolder.CopyHere(srcFolder.Items(), 16 | 256 | 512);
+
+            File.Delete(zipFile);
+
+
+            string destinationPath = Path.Combine(Application.StartupPath, "FirebirdSql.Data.FirebirdClient.dll");
+            File.Delete(destinationPath);
+            File.Move(Path.Combine(zipFileExtractionPath, "FirebirdSql.Data.FirebirdClient.dll"), destinationPath);
+
+            string basePath = Path.Combine(zipFileExtractionPath, "x86");
+            if (IntPtr.Size == 8)
+            {
+                basePath = Path.Combine(zipFileExtractionPath, "x64");
+            }
+
+            string[] remainingFiles = new string[] { "fbembed.dll", "icudt30.dll", "icuin30.dll", "icuuc30.dll", "ib_util.dll", "firebird.conf", "firebird.msg" };
+
+            foreach (string file in remainingFiles)
+            {
+                string fileToMove = Path.Combine(basePath, file);
+                destinationPath = Path.Combine(Application.StartupPath, file);
+                File.Delete(destinationPath);
+                File.Move(fileToMove, destinationPath);
+            }
+
+            Utility.DeleteDirectory(new DirectoryInfo(zipFileExtractionPath));
+        }
+
+
+
+        internal static void EnsureLabelDatabase()
+        {
+            EnsureFireBirdSetup();
+            using (FbConnection connection = new FbConnection(Atlas.MapLabelsDatabaseConnectionString))
+            {
+                connection.Open();
+
+                using (FbCommand command = new FbCommand())
+                {
+                    command.CommandText = "Select First 1 1 From Labels";
+                    command.Connection = connection;
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        return;
+                    }
+                    catch (FbException dbError)
+                    {
+                        if (!dbError.Message.Contains("SQL error code = -204\r\nTable unknown\r\nLABELS"))
+                        {
+                            throw;
+                        }
+                    }
+
+                    command.CommandText = "Create Table Labels(X SmallInt Not Null, Y SmallInt Not Null, Facet SmallInt Not Null, Category VarChar(1000) Not Null, Text VarChar(1000) Not Null);";
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+
+        internal static readonly Dictionary<string, LabelCategory> LabelCategories = new Dictionary<string, LabelCategory>();
+
+
+
+        internal static Label[] GetLabels(Rectangle bounds)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        internal static void DeleteAllLabels()
+        {
+            EnsureFireBirdSetup();
+            EnsureLabelDatabase();
+
+            using (FbConnection connection = new FbConnection(Atlas.MapLabelsDatabaseConnectionString))
+            {
+                connection.Open();
+
+                using (FbCommand command = new FbCommand())
+                {
+                    command.CommandText = "Delete From Labels";
+                    command.Connection = connection;
+                    command.ExecuteNonQuery();
+                }
             }
         }
     }
